7b5b6c561a2b84535abb74e4071443151e0f828f
suffynux <suffynux@gmail.com>
2024-11-18 17:52:32 +0500

    Changes done

--- Course Files/output/array.cpp
+++ Course Files/output/array.cpp
@@ -6,7 +6,7 @@ using namespace std;
 int main () {
 // Copying an Array
   // data should be 
-//   Initializing an Array
+//   Initializing an Array, firt we give the datatype then name and then size of array
   int age[] = {1 ,2 , 3 ,4 ,5, 6};
   int n[6];
   // using for loop for setting the age into second age
