49dc19a04af429d675c1aa3d62b98c353d25f9a6
suffynux <suffynux@gmail.com>
2024-11-18 17:29:24 +0500

    Array

--- Course Files/output/array.cpp
+++ Course Files/output/array.cpp
@@ -1,1 +1,19 @@
 // 
+#include <iostream>
+using namespace std;
+
+
+int main () {
+// Copying an Array
+  // data should be 
+//   Initializing an Array
+  int age[] = {1 ,2 , 3 ,4 ,5, 6};
+  int n[6];
+  // using for loop for setting the age into second age
+  for (int i= 0; i <6; i++){
+    // cout<< age[i] << endl;
+    
+    n[i] = age[i];
+    cout << n[i] ;
+  };
+}
